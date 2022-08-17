4a9600a7879349716e1d2b64a8c4433df4c73c25
Zach Boatwright <szboatwright@gmail.com>
2022-08-16 20:23:38 -0500

    basic outline

--- src/App.js
+++ src/App.js
@@ -2,7 +2,44 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return <div className="app">Hello World</div>
+
+  const facility = {
+    name: "Singleton Corp",
+    id:0,
+    facilities:[
+      {
+        name:"Atlanta",
+        coord:[33.749,-84.388],
+        id:1
+      },
+      {
+        name:"Memphis",
+        coord:[	35.117,-89.971],
+        id:2
+      },
+      {
+        name:"Boulder",
+        coord:[40.014,-105.270],
+        id:3
+      }
+    ]
+  }
+  
+  return (
+    <div className="app">
+      <header>
+        {facility.name}
+      </header>
+      <div className="body">
+        <div className="sidepanel">
+          side panel
+        </div>
+        <div className="map">
+          map
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
