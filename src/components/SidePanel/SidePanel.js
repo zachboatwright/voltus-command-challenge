c33b4e8b9c213d06da3ebda3d0683b0f0d99829c
Zach Boatwright <szboatwright@gmail.com>
2022-08-21 16:46:26 -0500

    add side panel functionality

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,15 +1,75 @@
-import React from 'react'
+import React, {useState} from 'react'
 import css from './SidePanel.module.css'
 
-function SidePanel() {
+function SidePanel({facilities}) {
 
+    const [facilityReadings, setFacilityReadings] = useState()
+
+    const updateFacilityReading = () => {
+        const id = document.getElementById('facility').value
+        const value = Number(document.getElementById('reading').value)
+        const newReadings = JSON.parse(JSON.stringify(facilityReadings))
+        newReadings[id] = value
+        setFacilityReadings(newReadings)
+    }
+
+    if (!facilityReadings) {
+        const readingsDefault = {}
+        facilities.forEach(facility => {
+            readingsDefault[facility.id] = 0
+        })
+        setFacilityReadings(readingsDefault)
+    }
+
+    if (!facilityReadings) return null
     return (
         <div className={css.sidepanel}>
-            <div className={css.panel}>
-                upper
+            <div className={`${css.panel} ${css.form}`}>
+                <select id="facility" aria-label="facility">
+                    <option hidden>Select</option>
+                    {
+                        facilities.map(facility => (
+                            <option key={facility.id} value={facility.id}>{facility.name}</option>
+                        ))
+                    }
+                </select>
+                <input type="number" id="reading" placeholder="kw" />
+                <button onClick={updateFacilityReading}>Submit</button>
             </div>
             <div className={css.panel}>
-                lower
+                <table className={css.table}>
+                    <thead>
+                        <tr>
+                            <th>Facility</th>
+                            <th>Reading</th>
+                            <th>Last Update</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            facilities.map(facility => {
+
+                                return (
+                                    <tr key={facility.id} className={css.row}>
+                                        <td>{facility.name}</td>
+                                        <td>{facilityReadings[facility.id]} kw</td>
+                                        <td>7/25/2018</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                        <tr className={css.row}>
+                            <td>All</td>
+                            <td>
+                                {Object.values(facilityReadings).reduce((prev, cur) =>{
+                                    return prev + cur
+                                } , 0)} 
+                                {' '}kw
+                            </td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     )
