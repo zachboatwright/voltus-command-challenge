da80caf8c5b7fcd85d81316dc1230f961f2d4eff
Zach Boatwright <szboatwright@gmail.com>
2022-08-21 14:42:13 -0500

    use real data source

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import Map from './components/map/Map'
 
 function App() {
 
-  const org = {
-    name: "Singleton Corp",
-    id:0,
-    facilities:[
-      {
-        name:"Atlanta",
-        coord:[33.749,-84.388],
-        id:1
-      },
-      {
-        name:"Memphis",
-        coord:[	35.117,-89.971],
-        id:2
-      },
-      {
-        name:"Boulder",
-        coord:[40.014,-105.270],
-        id:3
-      }
-    ]
-  }
+  const [org, setOrg] = useState(null)
+
+  const urlParams = new URLSearchParams(window.location.search)
+  const orgId = urlParams.get('organization_id')
+  fetch(`https://frontend-challenge.dev-cluster.voltus.co/facilities/${orgId}`)
+    .then(result => result.json())
+    .then(result => setOrg(result))
   
+  if (!org) return (
+    <div>
+      loading
+    </div>
+  )
   return (
     <div className="app">
       <header>
